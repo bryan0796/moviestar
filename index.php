bf02f052b0e43b34e2fb401138b93bcd2968e89f
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-10-06 20:08:42 -0300

    css header e footer

--- index.php
+++ index.php
@@ -49,20 +49,25 @@ include_once("db.php");
   </div>
 
   <footer id="footer">
-    <div class="social-container">
+    <div id="social-container">
       <ul>
         <li>
           <a href="#"><i class="fab fa-facebook-square"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-youtube"></i></a>
         </li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+
       </ul>
     </div>
     <div id="footer-links-container">
       <ul>
         <li>
           <a href="">Adicionar filme</a>
+        </li>
+        <li>
           <a href="">Adicionar crítica</a>
+        </li>
+        <li>
           <a href="">Entrar/Registrar</a>
         </li>
       </ul>
