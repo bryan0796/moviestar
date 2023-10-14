6dc9e7e87e0f6161b0ffcbdbd28a07cf189c8384
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-10-13 23:00:49 -0300

    senhas

--- auth_process.php
+++ auth_process.php
@@ -1,35 +1,48 @@
-<?php 
+<?php
 
-  require_once("globals.php");
-  require_once("db.php");
-  require_once("models/User.php");
-  require_once("models/Message.php");
-  require_once("dao/UserDAO.php");
+require_once("globals.php");
+require_once("db.php");
+require_once("models/User.php");
+require_once("models/Message.php");
+require_once("dao/UserDAO.php");
 
-  $message = new Message($BASE_URL);
+$message = new Message($BASE_URL);
+$userDao = new UserDAO($conn, $BASE_URL);
 
-  // Resgata o tipo do formulário
-  $type = filter_input(INPUT_POST, "type");
+// Resgata o tipo do formulário
+$type = filter_input(INPUT_POST, "type");
 
-  // Verificação do tipo de formulário
-  if($type === 'register') {
+// Verificação do tipo de formulário
+if ($type === 'register') {
 
-    $name = filter_input(INPUT_POST, 'name');
-    $lastname = filter_input(INPUT_POST, 'lastname');
-    $email = filter_input(INPUT_POST, 'email');
-    $password = filter_input(INPUT_POST, 'password');
-    $confirmpassword = filter_input(INPUT_POST, 'confirmpassword');
+  $name = filter_input(INPUT_POST, 'name');
+  $lastname = filter_input(INPUT_POST, 'lastname');
+  $email = filter_input(INPUT_POST, 'email');
+  $password = filter_input(INPUT_POST, 'password');
+  $confirmpassword = filter_input(INPUT_POST, 'confirmpassword');
 
-    // Verificação de dados mínimos
-    if($name && $lastname && $email && $password) {
-
+  // Verificação de dados mínimos
+  if ($name && $lastname && $email && $password) {
+    // Verificar se as senhas batem
+    if ($password === $confirmpassword) {
+      // Verificar se o email já está cadastrado
+      if ($userDao->findByEmail($email) === false) {
+        echo "nenhum usuário foi encontrado.";
+      } else {
+        // Enviar msg de erro, usuário já existe
+        $message->setMessage("Usuário já cadastrado, tente outro email.", "error", "back");
+      }
     } else {
-      // Enviar msg de erro de dados faltantes
-      $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
+      // Enviar msg de erro de, senhas não batem
+      $message->setMessage("As senhas não são iguais.", "error", "back");
     }
 
-  } else if($type === 'login') {
-
+  } else {
+    // Enviar msg de erro de dados faltantes
+    $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
   }
-  echo($type);
 
+} else if ($type === 'login') {
+
+}
+echo ($type);
