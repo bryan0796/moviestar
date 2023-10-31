e4f33ffc8fe7f815593a494a7a322685206f3c4f
Bryan de Palma Martins Vida <bpmv96@outlook.com>
2023-10-31 17:00:09 -0300

    css de filmes

--- movie_process.php
+++ movie_process.php
@@ -9,6 +9,7 @@ require_once("dao/MovieDAO.php");
 
 $message = new Message($BASE_URL);
 $userDao = new UserDAO($conn, $BASE_URL);
+$movieDao = new MovieDAO($conn, $BASE_URL);
 
 // Resgata o tipo do formulário
 $type = filter_input(INPUT_POST, "type");
@@ -35,6 +36,7 @@ if($type === "create") {
     $movie->trailer = $trailer;
     $movie->category = $category;
     $movie->length = $length;
+    $movie->users_id = $userData->id;
 
     // Upload de imagem do filme
     if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
